aad14f781a0a01b358257b0b3a68c806e9c9cf94
Garrett <gtcruz.gt@gmail.com>
2020-09-17 18:05:47 -0700

    Update cli.rb

--- redfin/lib/redfin/cli.rb
+++ redfin/lib/redfin/cli.rb
@@ -3,6 +3,7 @@ class Redfin::CLI
   def call
     list_homes
     menu
+    goodbye
   end
 
   def list_homes
@@ -22,21 +23,33 @@ class Redfin::CLI
 
   def menu
     puts ""
-    puts "Enter the number you want more information on"
-    input = gets.strip
-    case input
-    when "1"
-      puts "You selected Roseville"
-    when "2"
-      puts "You selected Auburn"
-    when "3"
-      puts "You selected Rocklin"
-    when "4"
-      puts "You selected Lincoln"
-    when "5"
-      puts "You selected Granite Bay"
-    when "6"
-      puts "You selected Loomis"
+    input = nil
+    while input != "exit"
+      puts "Enter the number you want more information on or type exit to leave, or list to see options again."
+      input = gets.strip
+      case input
+      when "1"
+        puts "You selected Roseville"
+      when "2"
+        puts "You selected Auburn"
+      when "3"
+        puts "You selected Rocklin"
+      when "4"
+        puts "You selected Lincoln"
+      when "5"
+        puts "You selected Granite Bay"
+      when "6"
+        puts "You selected Loomis"
+      when "list"
+        list_homes
+      else
+        puts "Your input is not valid, please input again"
+        list_homes
+      end
     end
+  end
+
+  def goodbye
+    puts "See you next time! :)"
   end
 end
